5bca320fe0328a88ab66bb28cf43e5ac199dea43
DimaChelnokov <dim.chelnokov@gmail.com>
2020-03-20 19:17:39 +0300

    Angular 2 Frontend

---
+++ frontend/src/app/user.service.ts
@@ -0,0 +1,26 @@
+﻿import { Injectable } from '@angular/core'
+import { HttpClient, HttpParams } from '@angular/common/http'
+import { User } from './user'
+
+@Injectable()
+export class UserService {
+  private url = 'http://127.0.0.1:3000/users'
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get(this.url)
+  }
+
+  createUser(user: User) {
+    return this.http.post(this.url, user)
+  }
+
+  updateUser(id: number, user: User) {
+    const urlParams = new HttpParams().set('id', id.toString())
+    return this.http.put(this.url, user, { params: urlParams })
+  }
+
+  deleteUser(id: number) {
+    return this.http.delete(this.url + '/' + id)
+  }
+}
